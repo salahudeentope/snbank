503c786ce47173921bb4beb4e139c21871a4483a
Tope Salahudeen <salahudeentope@gmail.com>
2020-05-09 18:24:19 +0100

    added extra functionality and the .txt files

--- snbank_code.py
+++ snbank_code.py
@@ -1,3 +1,13 @@
-import random 
+import random
 import os
-import sys
+import json
+
+start = True
+while start:
+    print("Welcome to Our-People Bank Plc Banking System \n")
+    print("1 Staff Login")
+    print("2 Close App")
+    start_option = input("Please enter your selection [1/2]: ")
+    while start_option not in ["1", "2"]:
+        start_option = input("kindly enter a valid selection [1 or 2]: ")
+    
